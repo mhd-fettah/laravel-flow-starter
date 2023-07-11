4caf13603fc2fb412426ff4c8a3a9a93e6f7a963
Fettah <2024designlab@gmail.com>
2023-07-12 02:49:36 +0300

    routes sample .

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,28 @@ Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+// Sample Group with auth
+/*Route::middleware(['auth', 'active'])->group(function() {
+
+    // Sample Controller
+    Route::controller(SampleController::class)->prefix('/item')->group(function () {
+        // Using Get method
+        Route::get('/', 'index')->name('sample.item.index');
+
+        // Using Post method
+        Route::post('/store', 'store')->name('sample.item.store');
+
+        // Using Delete method
+        Route::delete('/{item}/delete', 'delete')->name('sample.item.delete')->can('delete', 'item');
+
+        // Using Can ability
+        Route::get('/{item}/edit', 'edit')->name('sample.item.edit')->can('update', 'item');
+    });
+
+});*/
+
+
+// fall back route for any misspell
 Route::fallback(function () {
     return redirect('/');
 });

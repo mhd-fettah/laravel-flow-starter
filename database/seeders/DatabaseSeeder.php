2606af832931c8bf97449db796df8dbf9b134910
faissaloux <fwahabali@gmail.com>
2023-07-12 15:54:05 +0100

    refactor seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,18 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        if (! App::environment('production')) {
+            $this->call([
+                UserSeeder::class,
+                TestUserSeeder::class,
+            ]);
+        }
     }
 }
